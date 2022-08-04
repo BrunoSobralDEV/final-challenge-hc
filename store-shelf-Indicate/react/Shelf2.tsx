5e9c182819c7dc10200188aaba4c084a1b415def
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2022-08-04 03:16:04 -0300

    update/style

--- store-shelf-Indicate/react/Shelf2.tsx
+++ store-shelf-Indicate/react/Shelf2.tsx
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from 'react'
 import ShelfItem from "./ShelfItem";
 import { SliderLayout } from 'vtex.slider-layout'
 import { useCssHandles } from 'vtex.css-handles'
+import { useOrderItems } from 'vtex.order-items/OrderItems'
 
 const CSS_HANDLES = [
-   "shelfContainer",
-   "shelfTittle",
-   "add-to-cart-button"
+  "shelfContainer",
+  "shelfTitle",
+  "add-to-cart-button"
 ]
 
 const Shelf2 = () => {
+  const { addItems } = useOrderItems()
   const handles = useCssHandles(CSS_HANDLES)
   const [arrayProducts, setArrayProducts] = useState([]) as any
 
@@ -22,54 +24,124 @@ const Shelf2 = () => {
       .then(response => response.json())
       .then(data => {
         setArrayProducts(data)
-      });
-  }
-  
-  const addToCard = (event: any) => {
-
-    fetch('/api/catalog_system/pub/products/search/moda-feminina')
-      .then(response => response.json())
-      .then(data => {
-        setArrayProducts(data)
         console.log(data)
       });
-    console.log(event)
-    
-    
-
-
-
-    // window.location.href = '/checkout/#/cart'
-
-    
   }
+
+  const addToCard = (event: any) => {
+
+    const id = (event.target.id)
+    fetch(`/api/catalog_system/pub/products/search?fq=productId:${id}`)
+      .then(response => response.json())
+      .then(data => {
+        populateCart(data)
+        populateCart(data)
+
+      });
+  }
+  const addToCardCombo = (event: any) => {
+    console.log('adicinei combo')
+    const id = (event.target.id)
+    fetch(`/api/catalog_system/pub/products/search?fq=productId:${id}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        populateCartCombo(data)
+      });
+  }
+  const populateCart = (data: any) => {
+    const cart = [
+
+      {
+        additionalInfo: {
+          brandName: data[0].brand,
+          __typename: 'ItemAdditionalInfo',
+        },
+        availability: data[0].items[0].sellers[0].commertialOffer.IsAvaiable,
+        id: data[0].items[0].itemId,
+        imageUrls: {
+          altx: data[0].items[0].images[0].imageUrl,
+          __typename: 'ImageUrls',
+        },
+        listPrice: data[0].items[0].sellers[0].commertialOffer.ListPrice,
+        measurementUnit: data[0].items[0].measurementUnit,
+        name: data[0].productName,
+        price: data[0].items[0].sellers[0].commertialOffer.price,
+        productId: data[0].productId,
+        quantity: 1,
+        seller: data[0].items[0].sellers[0].sellerId,
+        sellingPrice: data[0].items[0].nameComplete,
+        unitMultiplier: data[0].items[0].unitMultiplier,
+        uniqueId: data[0].items[0].itemId,
+        isGift: false,
+        __typename: "Item",
+      }
+    ]
+
+    addItems(cart)
+  }
+  const populateCartCombo = (data: any) => {
+
+    const cart1 = [
+
+      
+      {
+        additionalInfo: {
+          brandName: data[1].brand,
+          __typename: 'ItemAdditionalInfo',
+        },
+        availability: data[1].items[0].sellers[0].commertialOffer.IsAvaiable,
+        id: data[1].items[1].itemId,
+        imageUrls: {
+          altx: data[1].items[0].images[0].imageUrl,
+          __typename: 'ImageUrls',
+        },
+        listPrice: data[1].items[1].sellers[0].commertialOffer.ListPrice,
+        measurementUnit: data[0].items[0].measurementUnit,
+        name: data[1].productName,
+        price: data[1].items[1].sellers[0].commertialOffer.price,
+        productId: data[1].productId,
+        quantity: 1,
+        seller: data[1].items[1].sellers[0].sellerId,
+        sellingPrice: data[1].items[1].nameComplete,
+        unitMultiplier: data[1].items[1].unitMultiplier,
+        uniqueId: data[1].items[1].itemId,
+        isGift: false,
+        __typename: "Item",
+      }
+    ]
+      
+    addItems(cart1)
+  }
+  
 
 
   return (<div className={`${handles.shelfContainer}`}>
-    <h1 className={`${handles.shelfTittle}`}>Produtos mais vendidos</h1>
+    <h1 className={`${handles.shelfTitle}`}>Produtos mais vendidos</h1>
 
     {arrayProducts ?
       <>
-      <SliderLayout
-      itemsPerPage={{
-        desktop: 2,
-        tablet: 1,
-        mobile: 1,
-      }}>
-      {arrayProducts.map((product: any) =>(
-          <ShelfItem
-          key={product.id}
-          id={product.id}
-          linkURL={product.link} 
-          imageURL={product.items[0].images[0].imageUrl} 
-          name={product.productName} 
-          sellingPrice={product.items[0].sellers[0].commertialOffer.ListPrice} 
-          price={product.items[0].sellers[0].commertialOffer.Price} 
-          addToCard={addToCard}
-          />
-        ))}
-      </SliderLayout>
-        
+        <SliderLayout
+          itemsPerPage={{
+            desktop: 3,
+            tablet: 2,
+            mobile: 1,
+          }}>
+          {arrayProducts.map((product: any) => (
+            <ShelfItem
+              key={product.id}
+              id={product.productId}
+              linkURL={product.link}
+              imageURL={product.items[0].images[0].imageUrl}
+              name={product.productName}
+              sellingPrice={product.items[0].sellers[0].commertialOffer.ListPrice}
+              price={product.items[0].sellers[0].commertialOffer.Price}
+              addToCard={addToCard}
+              addToCardCombo={addToCardCombo}
+            />
+          ))}
+        </SliderLayout>
+
       </>
       : ''}
 
--- store-shelf-Indicate/react/ShelfItem.tsx
+++ store-shelf-Indicate/react/ShelfItem.tsx
@@ -18,18 +18,20 @@ const CSS_HANDLES = [
     "shlefButtonAddToCard",
     "shelfPlus",
     "shelfItems",
-    "shelfMeddle"
+    "shelfMeddle",
+    "shelfCombo",
+    "shelfBestPriceCombo"
 
 
 ]
 
-const ShelfItem = ({ id, linkURL, imageURL, name, price, sellingPrice, addToCard }: any) => {
+const ShelfItem = ({ id, linkURL, imageURL, name, price, sellingPrice, addToCard, addToCardCombo }: any) => {
 
     const handles = useCssHandles(CSS_HANDLES)
 
     return (
         <>
-                
+
             <div className={`${handles.shelfItems}`}>
 
                 <div key={id} className={`${handles.shelfItem}`}>
@@ -37,40 +39,60 @@ const ShelfItem = ({ id, linkURL, imageURL, name, price, sellingPrice, addToCard
                         <div className={`${handles.shelfImages}`}>
                             <img src={`${imageURL}`} alt={`${name}`} className={`${handles.shelfImage}`} />
                         </div>
-                        <h2 className={`${handles.shelfProductName}`}>{`${name}`}</h2>
-                        <div className={`${handles.shelfPrice}`}>
-                            <p className={`${handles.shelfSellingPrice}`}>
-                                {formatPrice(sellingPrice)}
-                            </p>
-                            <p className={`${handles.shelfBestPrice}`}>
-                                {formatPrice(price)}
-                            </p>
-                        </div>
                     </a>
+                    <h2 className={`${handles.shelfProductName}`}>{`${name}`}</h2>
+                    <div className={`${handles.shelfPrice}`}>
+                        <p className={`${handles.shelfSellingPrice}`}>
+                            {formatPrice(sellingPrice)}
+                        </p>
+                        <p className={`${handles.shelfBestPrice}`}>
+                            {formatPrice(price)}
+                        </p>
+                    </div>
+                    <button className={`${handles.shlefButtonAddToCard}`} id={id} onClick={addToCard}>
+                        Adicione ao carrinho
+                    </button>
+
                 </div>
                 <div className={`${handles.shelfMeddle}`}>
                     <h1 className={`${handles.shelfPlus}`}>+</h1>
-                    <button className={`${handles.shlefButtonAddToCard}`} id={id} onClick={addToCard}>
-                        Adicionar este combo ao carrinho
-                    </button>
                 </div>
                 <div key={id} className={`${handles.shelfItem}`}>
                     <a href={`${linkURL}`} className={`${handles.shelfLink}`}>
                         <div className={`${handles.shelfImages}`}>
                             <img src={`${imageURL}`} alt={`${name}`} className={`${handles.shelfImage}`} />
                         </div>
-                        <h2 className={`${handles.shelfProductName}`}>{`${name}`}</h2>
-                        <div className={`${handles.shelfPrice}`}>
-                            <p className={`${handles.shelfSellingPrice}`}>
-                                {formatPrice(sellingPrice)}
-                            </p>
-                            <p className={`${handles.shelfBestPrice}`}>
-                                {formatPrice(price)}
-                            </p>
-                        </div>
                     </a>
+                    <h2 className={`${handles.shelfProductName}`}>{`${name}`}</h2>
+                    <div className={`${handles.shelfPrice}`}>
+                        <p className={`${handles.shelfSellingPrice}`}>
+                            {formatPrice(sellingPrice)}
+                        </p>
+                        <p className={`${handles.shelfBestPrice}`}>
+                            {formatPrice(price)}
+                        </p>
+                    </div>
+
+                    <button className={`${handles.shlefButtonAddToCard}`} id={id} onClick={addToCard}>
+                        Adicione ao carrinho
+                    </button>
+
                 </div>
 
+            </div>
+            <div className={`${handles.shelfMeddle}`}>
+                <h1 className={`${handles.shelfPlus}`}>=</h1>
+            </div>
+
+            <div className={`${handles.shelfCombo}`}>
+
+                <p className={`${handles.shelfBestPriceCombo}`}>
+                    {formatPrice(price + price)}
+                </p>
+
+                <button className={`${handles.shlefButtonAddToCard}`} id={id} onClick={addToCardCombo}>
+                    Adicionar este <br/> combo ao carrinho
+                </button>
             </div>
 
         </>
