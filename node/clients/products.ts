8d6a6f20bd2c81cb71afe2b69277cb4a48ddcce7
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2022-08-04 21:59:30 -0300

    Update API url and required parameters

--- node/clients/products.ts
+++ node/clients/products.ts
@@ -4,14 +4,14 @@ import { ExternalClient } from "@vtex/api";
 export default class Products extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
     super(
-      "https://3jfc0467k2.execute-api.us-east-1.amazonaws.com",
+      "https://k30cnnmuxj.execute-api.us-east-1.amazonaws.com",
       context,
       options
     );
   }
 
-  public async saveProducts(products: Product[]) {
-    return this.http.post("/compra", products, {
+  public async saveProducts(order: OrderInfo) {
+    return this.http.post("/createOrUpdate/combinationCreateOrUpdate", order, {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
