4711c3df7f87c15fefdeb52cdae525d82dce339a
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2022-08-05 12:37:02 -0300

    README

---
+++ store-shelf-Indicate/react/checkout/UseCheckout.js
@@ -0,0 +1,17 @@
+import { useCheckoutURL } from 'vtex.checkout-resources/Utils'
+import { useRuntime } from 'vtex.render-runtime'
+
+export default function useCheckout() {
+  const { url: checkoutUrl, major } = useCheckoutURL()
+  const { navigate, rootPath = '' } = useRuntime()
+
+  const goToCheckout = (url) => {
+    if (major > 0 && url === checkoutUrl) {
+      navigate({ to: url })
+    } else {
+      window.location.href = `${rootPath}${url}`
+    }
+  }
+
+  return goToCheckout
+}
